94c922b05b885e878acab7dd355f8b6a3ea71742
wang <nmgwbd@163.com>
2021-03-14 16:02:58 +0800

    fix file list

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+int main() {
+    std::cout << "Hello world" << std::endl;
+    return 0;
+}
