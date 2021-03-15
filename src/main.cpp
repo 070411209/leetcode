d29a82e6264e8fa2d0e725e36ad60fcb48e1ef76
wang <nmgwbd@163.com>
2021-03-15 08:49:21 +0800

    add new

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 
+using namespace std;
 // @lc code=start
 /**
  * Definition for singly-linked list.
@@ -11,7 +13,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+
+int fib(int N) {
+    if (N == 1 || N == 2) return 1;
+    vector<int> dp(N+1, 0);
+    dp[1] = dp[2] = 1;
+    for(auto i = 3; i < N; i++) {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+    return dp[N];
+}
+
+
 int main() {
     std::cout << "Hello world" << std::endl;
+    clock_t start, end;
+    start = clock();
+    int ret = fib(20);
+    end = clock();
+    cout << "Take Time : " << (double)(end - start) << "ms" << endl;
+    std::cout << "FIB = " <<  ret << std::endl;
     return 0;
 }
